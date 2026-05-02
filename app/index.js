4f41e82d7c832f522896fd4a8cba6616424df6b5
Navbila K <navbilak@gmail.com>
2026-05-02 21:37:50 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,1 +1,10 @@
-console.log("Hello CI/CD Pipeline");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from Docker CI/CD 🚀");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
